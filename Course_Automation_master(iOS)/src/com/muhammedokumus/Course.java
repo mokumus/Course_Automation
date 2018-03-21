e732ce6a43d297a39386c97ff9f52018ef929572
Muhammed Okumuş <rocketpolka@gmail.com>
2018-03-21 11:32:15 +0300

    Add files via upload

--- Course_Automation_master(iOS)/src/com/muhammedokumus/Course.java
+++ Course_Automation_master(iOS)/src/com/muhammedokumus/Course.java
@@ -5,7 +5,7 @@ public class Course {
     private String courseCode;
     private String courseTitle;
     private int ectsCredit;
-    private int gtuCredit;
+    private double gtuCredit;
     private String creditWeight;
 
     public Course(){
@@ -32,15 +32,33 @@ public class Course {
                 "T+U+L=" + creditWeight +
                 "]";
     }
+    //SETTERS
+    private void setSemester(int semester){ this.semester = semester; }
 
-    public void setSemester(int semester){
-        this.semester = semester;
-    }
+    private void setCourseCode(String courseCode) { this.courseCode = courseCode; }
 
+    private void setCourseTitle(String courseTitle) { this.courseTitle = courseTitle; }
+
+    private void setEctsCredit(int ectsCredit) { this.ectsCredit = ectsCredit; }
+
+    private void setGtuCredit(double gtuCredit) { this.gtuCredit = gtuCredit; }
+
+    private void setCreditWeight(String creditWeight) { this.creditWeight = creditWeight; }
+
+
+
+    //GETTERS
     public int getSemester(){
         return semester;
     }
 
+    public String getCourseCode(){ return courseCode; }
 
+    public String getCourseTitle(){ return courseTitle; }
 
+    public int getEctsCredit(){ return ectsCredit; }
+
+    public double getGtuCredit() { return gtuCredit; }
+
+    public String getCreditWeight() { return creditWeight; }
 }
